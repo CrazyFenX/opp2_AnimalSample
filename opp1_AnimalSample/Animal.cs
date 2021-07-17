accb4497d96b57dcb5c56f9b7c8daa9b88587075
Кирилл <kattoshkin@gmail.com>
2021-07-17 16:49:58 +0700

    Добавьте файлы проекта.

---
+++ opp1_AnimalSample/Animal.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opp1_AnimalSample
+{
+    public class Animal
+    {
+        //поля
+        public string name;    //кличка
+        public float mass = 0; //вес
+        public char color;     //цвет
+    
+        //конструкторы
+        public Animal() { }
+        public Animal(string name1, float mass1, char color1)
+        {
+            name = name1;
+            mass = mass1;
+            color = color1;
+        }
+        public Animal(string name, float mass)
+        {
+            this.name = name;
+            this.mass = mass;
+        }
+
+        //методы
+        public virtual void say()
+        {
+            Console.WriteLine($"{name} ({color}, {mass}) says:");
+            Console.WriteLine("Abstract voice"); //издать абстрактный звук
+        }
+    }
+
+    class Cat : Animal
+    {
+        //Поля
+        public float lengthTail;
+
+        //Конструкторы
+        //public Cat(string name1, float mass1, char color1, float lengthTail1)
+        //{
+        //    name = name1;
+        //    mass = mass1;
+        //    color = color1;
+        //    lengthTail = lengthTail1;
+        //}
+
+        public Cat(string name1, float mass1, char color1, float lengthTail1) : base(name1, mass1, color1)
+        {
+            lengthTail = lengthTail1;
+        }
+
+        //методы
+        public override void say()
+        {
+            Console.WriteLine($"{name} ({color}, {mass}, {lengthTail}) says:");
+            Console.WriteLine("meow"); //издать звук meow
+        }
+    }
+
+    class Dog : Animal
+    {
+        //Конструкторы
+        public Dog(string name1, float mass1, char color1) : base(name1, mass1, color1) { }
+
+        //Методы
+        public override void say()
+        {
+            Console.WriteLine($"{name} ({color}, {mass}) says:");
+            Console.WriteLine("wof"); //издать звук wof
+        }
+    }
+}
---
+++ opp1_AnimalSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace opp1_AnimalSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Animal cat = new Animal("Pushok", 2.3f, 'b');
+            ////cat.color = 'w';
+            ////cat.name = "pushok";
+            ////cat.mass = 2.3f;
+            //cat.say();
+
+            Cat cat = new Cat("Pushok", 2.3f, 'b', 0.4f);
+            Dog dog = new Dog("Sharik", 5.2f, 'g');
+
+            cat.say();
+            Console.WriteLine();
+            dog.say();
+        }
+    }
+}
